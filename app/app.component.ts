28cecd269709ab4100a1aaf98191ae2f1df6b56e
denkov <denkov@scavaline.com>
2016-09-19 17:44:48 +0300

    Authors component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,13 @@
 import {Component} from 'angular2/core';
-import {CoursesComponent} from './courses.component'
-
+import {CoursesComponent} from './courses.component';
+import {AuthorComponent} from './author.component';
+ 
 @Component({
     selector: 'my-app',
-    template: '<h1>Hello Angular 2</h1><courses></courses>',
-    directives: [CoursesComponent]
+    template: `<h1>Hello Angular 2</h1>
+    <courses></courses>
+    <authors></authors>`,
+    directives: [CoursesComponent, AuthorComponent]
 })
+
 export class AppComponent { }
--- app/author.component.js
+++ app/author.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', './course.service'], function(exports_1, context_1) {
+System.register(['angular2/core', './author.service'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,34 +10,34 @@ System.register(['angular2/core', './course.service'], function(exports_1, conte
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, course_service_1;
-    var CoursesComponent;
+    var core_1, author_service_1;
+    var AuthorComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
             },
-            function (course_service_1_1) {
-                course_service_1 = course_service_1_1;
+            function (author_service_1_1) {
+                author_service_1 = author_service_1_1;
             }],
         execute: function() {
-            CoursesComponent = (function () {
-                function CoursesComponent(courseService) {
-                    this.title = "The title of the course page";
-                    this.courses = courseService.getCourses();
+            AuthorComponent = (function () {
+                function AuthorComponent(authorService) {
+                    this.title = "The title for the authors page";
+                    this.authors = authorService.getAuthors();
                 }
-                CoursesComponent = __decorate([
+                AuthorComponent = __decorate([
                     core_1.Component({
-                        selector: 'courses',
-                        template: "\n    <h2>Courses</h2>\n    {{ title }}\n    <ul>\n      <li *ngFor=\"#course of courses\">\n      {{ course }}\n      </li>\n    </ul>\n    ",
-                        providers: [course_service_1.CourseService]
+                        selector: 'authors',
+                        template: "\n        <h2>Authors</h2>\n        {{ title }}\n        <ul>\n              <li *ngFor=\"#author of authors\">\n            {{ author }}\n            </li>\n        </ul>\n    ",
+                        providers: [author_service_1.AuthorService]
                     }), 
-                    __metadata('design:paramtypes', [course_service_1.CourseService])
-                ], CoursesComponent);
-                return CoursesComponent;
+                    __metadata('design:paramtypes', [author_service_1.AuthorService])
+                ], AuthorComponent);
+                return AuthorComponent;
             }());
-            exports_1("CoursesComponent", CoursesComponent);
+            exports_1("AuthorComponent", AuthorComponent);
         }
     }
 });
-//# sourceMappingURL=courses.component.js.map
+//# sourceMappingURL=author.component.js.map
