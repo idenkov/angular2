72fc9a73e3050f79422a7bf81214147856e1e407
denkov <denkov@scavaline.com>
2016-09-21 15:27:49 +0300

    ng-content

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,23 +1,16 @@
 import {Component} from 'angular2/core';
-import {SummaryPipe} from './summary.pipe';
+import {BootstrapPanel} from './bootstrap.panel.component';
 
 @Component({
     selector: 'my-app',
+    directives: [BootstrapPanel],
     template: `
-        {{ post.title }}
-        <br />
-        {{ post.body | summary:25 }}
+        <bs-panel>
+            <div class="heading">The Heading</div>
+            <div class="body">This is the body!</div>
+            <div class="body">This is another body!</div>
+        </bs-panel>
     `,
-    pipes: [SummaryPipe]
 })
 export class AppComponent {
-    post = {
-        title: "Angular Tutorial for Beginners",
-        body: `
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In malesuada scelerisque tristique. 
-            Curabitur mattis dui mi, et dapibus odio mollis quis. Praesent aliquet lectus id gravida suscipit. 
-            Fusce volutpat efficitur vulputate. Nunc porttitor aliquam fringilla. Donec quis lectus tristique,
-             consectetur mi non, rhoncus dolor. Phasellus elementum mollis finibus.
-        `    
-    }
 }
