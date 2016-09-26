1b13dc1d489b8530a575137bc76ae2471be669c7
denkov <denkov@scavaline.com>
2016-09-26 13:38:55 +0300

    Disabling submit button

--- app/contact-form.component.ts
+++ app/contact-form.component.ts
@@ -5,8 +5,7 @@ import {Component} from 'angular2/core';
     templateUrl: 'app/contact-form.template.html' 
 })
 export class ContactFormComponent {
-    log(x){
-        console.log(x);
-    }
-    
+    onSubmit(form){
+        console.log(form);
+    }    
 }
